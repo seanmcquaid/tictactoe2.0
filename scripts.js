f335798e6d04fcd070d9589b99168e53fa1201c4
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-12 22:16:55 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// start the new grid
+
+let gridSize = 5;
+// for loop to create gridsize rows 
+
+function makeRows(gridSize){
+    let rowArray = [];
+    for(let i = 0; i < gridSize; i++){
+
+    }
+}
+
+// use loop to create gridsize amount of divs for rows and buttons for each row and 
+// create event listeners for each button
+// event listeners in for loop will change style and html in each button
+
+// function to check winner
+// function to check for tie
+// function to generate computer turn
